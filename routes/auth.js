8b0f71a4b4a220eff11e4c4755c08931279d6bb8
RP Boyle <rpboyle11@yahoo.com>
2018-12-04 15:12:03 -0500

    updated users.js file

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const jsonwebtoken = require('jsonwebtoken');
+const { JWT_SECRET, JWT_EXPIRY } = require('./config');
 
 const router = express.Router();
 
@@ -13,5 +15,12 @@ const localAuth = passport.authenticate('local', options);
 router.post('/login', localAuth, function (req, res) {
   return res.json(req.user);
 });
+
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -9,10 +9,12 @@ const router = express.Router();
 
 router.post('/', (req, res, next) => {
   const { username, password } = req.body;
-  const requiredFields = ['username', 'password'];
-  const missingField = requiredFields.find(field => !(field in req.body));
-
+  
   const newUser = { username, password };
+  
+  const requiredFields = ['username', 'password'];
+  
+  const missingField = requiredFields.find(field => !(field in req.body));
 
   /***** Never trust users - validate input *****/
   if (missingField) {
@@ -21,9 +23,53 @@ router.post('/', (req, res, next) => {
     return next(err);
   }
 
-  if (typeof newUser.username !== 'string' || typeof newUser.password !== 'string') {
-    const err = new Error('username and password must be string');
+  const stringFields = ['username', 'password', 'fullname'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    const err = new Error(`Field: '${nonStringField}' must be type string.`);
     err.status = 415;
+    return next(err);
+  }
+
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    const err = new Error(`Field: '${nonTrimmedField}' cannot start or end with whitespace.`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const sizedFields = {
+    username: { min: 1 },
+    password: { min: 8, max: 72 }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+  if (tooSmallField) {
+    const min = sizedFields[tooSmallField].min;
+    const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooLargeField) {
+    const max = sizedFields[tooLargeField].max;
+    const err = new Error(`Field: '${tooLargeField}' must be at most ${max} characters long`);
+    err.status = 422;
     return next(err);
   }
 
@@ -33,7 +79,7 @@ router.post('/', (req, res, next) => {
     return next(err);
   }
 
-  User.hashPassword(password)
+  return User.hashPassword(password)
     .then(digest => {
       const newUser = {
         username,
